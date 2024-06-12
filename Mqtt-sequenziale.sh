e9512293985ff6d7f13864a3b3947d1d405c0313
Argo79 <melloni6@gmail.com>
2024-06-12 19:37:29 +0200

    Update Mqtt-sequenziale.sh
    Cleaning

--- Mqtt-sequenziale.sh
+++ Mqtt-sequenziale.sh
@@ -1,13 +1,17 @@
 #! /bin/bash
+# Programma che avvia lo script Mqtt2prova.sh che attiva sottoscrizioni ad un broker a determinati topic configurabili in Mqtt2prova.sh che salva in singoli file il timestamp e il payload di ogni topic
+# A questo punto Mqtt-sequenziale prosegue manipolando un po' i dati e genera un file wxnow.txt pronto per il programma Direwolf che lo può inviare alla rete.
+
+
 broker_ip="192.168.1.11"
-valori=()
 
 dir="/home/kevin/blobspace/mqtt2wxnow"
 
 $dir/./Mqtt2prova.sh &
 
-sleep 15
+sleep 45 # tempo di esecuzione di mosquitto_sub. Il temnpo dipende anche dalla frequenza di aggiornamento dei dati mqtt
 
+# carica i valori dei singoli files
 temp=$(<$dir/temp.txt)
 press=$(<$dir/press.txt)
 umi=$(<$dir/umi.txt)
@@ -16,33 +20,34 @@ valori[0]=$temp
 valori[1]=$press
 valori[2]=$umi
 
-
+# Legge la temperatura
 IFS=':' read -a data <<< "${valori[0]}"
 timeT=${data[0]}
 tempC=${data[1]}
-echo $timeT
-echo $tempC
-tempF=$(echo "scale=1; $tempC*1.8+32" | bc -l)
+#echo $timeT
+#echo $tempC
+tempF=$(echo "scale=1; $tempC*1.8+32" | bc -l)  # crea temperatura Farhenheit
 tempFA=${tempF%%.*}
 echo $tempF
 #echo $tempFA
 
-
+# Legge la pressione
 IFS=':' read -a data <<< "${valori[1]}"
 timeP=${data[0]}
 press=${data[1]}
-echo $timeP
-echo $press
+#echo $timeP
+#echo $press
 pressM=$(echo "scale=0; $press * 10" | bc -l)
 pressA=${pressM%%.*}
 
-
+# Legge l'umidità
 IFS=':' read -a data <<< "${valori[2]}"
 timeU=${data[0]}
 umi=${data[1]}
-echo $timeU
-echo $umi
+#echo $timeU
+#echo $umi
 umiA=${umi%%.*}
+
 
 if [[ $tempFF -lt 100 ]]
 then
@@ -53,10 +58,10 @@ fi
 
 
 today=$(/bin/date "+%b %d %Y %H:%M")    #Used to generate filename
-echo ${today^}
+#echo ${today^}
 echo ${today^} > $dir/wxnow.txt
 msg=$(echo $inizio$tempFA"r...p...P...h"$umiA"b"$pressA)
-echo $msg
+#echo $msg
 echo $msg >> $dir/wxnow.txt
 
 sleep 5
